dd7d3362e968d28678562014e4a1dc3ecdc1b8b7
alan ronald <alanronald787@gmail.com>
2025-04-18 12:31:28 +0530

    refactor: added dom

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
+const button = document.querySelectorAll("button");
+const result = document.getElementById("result");
+const humanPoints = document.getElementById("human-score");
+const computerPoints = document.getElementById("computer-score");
+
+let humanScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
   if (randomNumber === 0) return "rock";
   if (randomNumber === 1) return "paper";
   return "scissors";
 };
-const getHumanChoice = () => {
-  const humanChoice = prompt();
-  return humanChoice;
+
+const reset = () => {
+  humanScore = 0;
+  computerScore = 0;
+  result.innerText = "";
 };
 
-const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
-  const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === "rock" && computerChoice === "scissors") {
-      humanScore += 1;
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-      computerScore += 1;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-      humanScore += 1;
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-      computerScore += 1;
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-      computerScore += 1;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      humanScore += 1;
-    }
-  };
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
+const playRound = (humanChoice) => {
+  const computerChoice = getComputerChoice();
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1;
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1;
   }
-  if (humanScore < computerScore)
-    return `computer wins ${computerScore}, ${humanScore}`;
-  else return `human wins ${humanScore}, ${computerScore}`;
+  humanPoints.innerText = humanScore;
+  computerPoints.innerText = computerScore;
+  if (humanScore >= 5) {
+    reset();
+    result.innerText = "Human Wins";
+  }
+  if (computerScore >= 5) {
+    reset();
+    result.innerText = "Computer Wins";
+  }
 };
 
-console.log(playGame());
+button.forEach((btn) => {
+  const option = btn.getAttribute("data-option");
+  btn.addEventListener("click", () => playRound(option));
+});
